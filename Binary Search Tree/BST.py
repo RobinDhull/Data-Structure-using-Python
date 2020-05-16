fe7b2ebd6e97ce84855de259afe8b959aa9fefdf
Robin Dhull <robin707singh@gmail.com>
2020-05-16 10:58:30 +0530

    Add files via upload

---
+++ Binary Search Tree/BST.py
@@ -0,0 +1,46 @@
+# Q1. 
+class Node: 
+	def __init__(self, value): 
+		self.left = None
+		self.right = None
+		self.val =  value 
+
+def insert(root, node): 
+	if (root == None): 
+		root = node 
+	else: 
+		if (root.val < node.val): 
+			if (root.right == None): 
+				root.right = node 
+			else: 
+				insert(root.right, node) 
+		else: 
+			if (root.left == None): 
+				root.left = node 
+			else: 
+				insert(root.left, node) 
+ 
+def inorderTraversal(root): 
+	if (root != None): 
+		inorderTraversal(root.left) 
+		print(root.val) 
+		inorderTraversal(root.right)
+
+def Search(root, value):  
+    if (root == None):
+        print(root)
+        return
+    elif (root.val == value): 
+        print(root.val, 'Found')
+        return
+    elif (root.val < value): 
+        return Search(root.right, value)
+
+    return Search(root.left, value)  
+
+list_ = ['P', 'V', 'S', 'C', 'M', 'Y']
+r = Node('A')
+for i in list_:
+    insert(r,Node(i))
+
+Search(r, 'D')
